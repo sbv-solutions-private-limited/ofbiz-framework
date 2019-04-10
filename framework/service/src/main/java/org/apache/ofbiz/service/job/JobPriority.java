fc78876329f2d32c1c40d65aec7c659ccc7a3848
sri.jasti <jastisriram94@gmail.com>
2019-04-10 11:42:25 +0530

    working branch with lot of changes to trunk

--- framework/service/src/main/java/org/apache/ofbiz/service/job/JobPriority.java
+++
@@ -1,7 +0,0 @@
-package org.apache.ofbiz.service.job;
-
-public final class JobPriority {
-    public static final long LOW = 0;
-    public static final long NORMAL = 50;
-    public static final long HIGH = 100;
-}
